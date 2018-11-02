be82afdaab92b5a66e3734e3aff452752687b98d
Samantha Dinh <samantha.dinh7@gmail.com>
2018-11-02 18:31:46 +0100

    base spring

---
+++ src/main/java/sdn/flicker/RandomImageFlickerApplication.java
@@ -0,0 +1,12 @@
+package sdn.flicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomImageFlickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomImageFlickerApplication.class, args);
+	}
+}
